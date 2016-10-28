ad1557d7ca4f6042badb1b3f5da5588118da245a
Catherine Khuu & Jacob Williams & Lindsay Kelly & Nan Min <pair+catkhuu.jacobgwilliams.lindsaymkelly.nanutza@devbootcamp.com>
2016-10-28 15:33:26 -0400

    Add logic for rounds controller.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,7 +2,7 @@ class Deck < ActiveRecord::Base
 
   has_many :rounds
   has_many :cards
-  has_many :players, through: rounds, source: user
+  # has_many :players, through: rounds, source: user
 
   validates :subject, presence: true
 
