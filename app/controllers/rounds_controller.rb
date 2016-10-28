88ad1377f6d169ba36b3e0cde9c095674686e648
Catherine Khuu & Jacob Williams & Lindsay Kelly & Nan Min <pair+catkhuu.jacobgwilliams.lindsaymkelly.nanutza@devbootcamp.com>
2016-10-28 18:02:32 -0400

    added game playing routes

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -5,19 +5,26 @@ end
 
 get '/decks/:deck_id' do
   @deck = Deck.find_by(id: params[:deck_id])
-  @cards = @deck.cards
+  @round = Round.new(user_id: current_user.id, deck_id: @deck.id).save unless @round
+  @cards = []
+  @deck.cards.each do |card|
+    @cards << card unless card.guessed_correctly == 0
+  end
   @card = @cards.sample
-  # binding.pry
   erb :'decks/show'
 end
 
 post '/decks' do
   @answer = params[:answer]
   @deck_id = params[:deck_id]
+  @round = Round.last
   card = Card.find_by(answer: params[:answer])
+  binding.pry
+  guess = Guess.new(round_id: @round.id, card_id: card.id)
   if @answer == card.answer
+    card.guessed_correctly = 1
+    card.update_attributes(params[:guessed_correctly])
     puts "Correct!"
-
   else
     puts "Did not see answer"
     puts params
