7e5b14788832489bf2011d01d380d9da5d3e30fe
Catherine Khuu & Lindsay Kelly <pair+catkhuu.lindsaymkelly@devbootcamp.com>
2016-10-28 14:33:35 -0400

    Add validations for all models

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,4 +2,15 @@ class Guess < ActiveRecord::Base
   belongs_to :card
   belongs_to :round
   has_many :players, through: :round, source: :user, class_name: :User
+
+  validates_presence_of :card_id, :round_id
+  validate :validate_card_id, :validate_round_id
+
+  def validate_card_id
+    errors.add(:card_id, "does not exist") unless Card.exists?(self.card_id)
+  end
+
+  def validate_round_id
+    errors.add(:round_id, "does not exist") unless Round.exists?(self.round_id)
+  end
 end
