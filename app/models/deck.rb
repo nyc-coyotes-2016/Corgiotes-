079bb6670c0b1f1fbabe61cd838f37d375460ee7
Catherine Khuu & Jacob Williams & Lindsay Kelly & Nan Min <pair+catkhuu.jacobgwilliams.lindsaymkelly.nanutza@devbootcamp.com>
2016-10-28 13:08:06 -0400

    Update associations

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,7 +1,7 @@
 class Deck < ActiveRecord::Base
   has_many :rounds
   has_many :cards
-  has_many :players, through: rounds, source: user
+  # has_many :players, through: :rounds, source: :player  #FIX ME !!!!
   # belongs_to :creator, class: User
   validates :subject, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   has_many :rounds
   has_many :guesses, through: :rounds, source: :guesses
   has_many :played_cards, through: :guesses, source: :card
+  has_many :played_decks, through: :rounds, source: :deck
 end
