6ee14dbe74e4766e0c94bf8c046d2bfbd5dc4842
Catherine Khuu & Lindsay Kelly <pair+catkhuu.lindsaymkelly@devbootcamp.com>
2016-10-28 16:51:55 -0400

    Create logic for user stats in dashboard.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -4,4 +4,17 @@ class Deck < ActiveRecord::Base
   # has_many :players, through: :rounds, source: :player  #FIX ME !!!!
   # belongs_to :creator, class: User
   validates :subject, presence: true
+
+  def played_at
+    self.rounds.map { |round| round.created_at.strftime('%B %d, %Y') }
+  end
+
+  def card_quantity
+    self.cards.count
+  end
+
+  def first_guess_correct
+    self.rounds.map { |round| round.guesses.}
+  end
+
 end
