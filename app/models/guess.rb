8c17c9d8dd13ee33886eeabe6961a707bf8ad18c
Catherine Khuu & Jacob Williams & Lindsay Kelly & Nan Min <pair+catkhuu.jacobgwilliams.lindsaymkelly.nanutza@devbootcamp.com>
2016-10-28 12:48:17 -0400

    model asscociations and validations

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -4,7 +4,6 @@ class Guess < ActiveRecord::Base
   belongs_to :round
   has_many :players, through: :round, source: :user, class_name: :User
 
-
   validates_presence_of :card_id, :round_id
   validate :validate_card_id, :validate_round_id
 
