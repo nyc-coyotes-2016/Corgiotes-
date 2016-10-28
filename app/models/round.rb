71a77ce3011e5ef572d65f43e6280a48d32f4c68
Catherine Khuu & Jacob Williams & Lindsay Kelly & Nan Min <pair+catkhuu.jacobgwilliams.lindsaymkelly.nanutza@devbootcamp.com>
2016-10-28 13:08:06 -0400

    Update associations

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,8 +1,9 @@
 class Round < ActiveRecord::Base
 
   has_many :guesses
-  belongs_to :user
-  belongs_to :deck
   has_many :guessed_cards, through: :guesses, source: :card
+
+  belongs_to :player, class_name: 'User'
+  belongs_to :deck
 
 end
